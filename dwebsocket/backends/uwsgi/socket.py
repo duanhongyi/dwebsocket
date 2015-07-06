3c57429338af36d0ce121e64b0cc542c7f822a54
duanhongyi <duanhongyi@doopai.com>
2015-07-06 15:11:04 +0800

    fix uwsgi websocket bug

--- dwebsocket/backends/uwsgi/socket.py
+++ dwebsocket/backends/uwsgi/socket.py
@@ -5,18 +5,23 @@ class Socket(object):
 
     def __init__(self, request):
         self.request = request
+        self.closed = False
 
 
     def fileno(self):
         return self.request.META["wsgi.input"].fileno()
 
     def recv(self, bufsize):
-        uwsgi.wait_fd_read(self.fileno(), 300)
-        uwsgi.suspend()
-        return uwsgi.recv(self.fileno(), bufsize)
+        if not self.closed:
+            uwsgi.wait_fd_read(self.fileno(), 300)
+            uwsgi.suspend()
+        if not self.closed:
+            return uwsgi.recv(self.fileno(), bufsize)
 
     def send(self, body):
-        return uwsgi.send(self.fileno(), body)
+        if not self.closed:
+            return uwsgi.send(self.fileno(), body)
 
     def close(self):
+        self.closed = True
         uwsgi.close(self.fileno())
--- examples/examples/wsgi.py
+++ examples/examples/wsgi.py
@@ -27,6 +27,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "examples.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
+
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
